35eff398a740a8be3ae653ec88f14ab128fe77bc
vinay-sridhar <vinay.s@research.iiit.ac.in>
2021-06-30 23:10:08 +0530

    gallery complete , dynamic table pending

--- js/page4.js
+++ js/page4.js
@@ -1,11 +1,27 @@
 function addRow() {
     var table = document.getElementById("addTable");
+    var tr = document.createElement('tr');
+
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    var td4 = document.createElement('td');
+
     var rowCount = table.rows.length;
-    var addRow = table.insertRow(rowCount);
-    var cell1 = addRow.insertCell(0);
-    cell1.innerHTMl = rowCount + 1;
-    addRow.insertCell(1);
-    addRow.insertCell(2);
-    addRow.insertCell(3);
+    var number = document.createTextNode(rowCount);
+    var input1 = document.createTextNode('Text1');
+    var input2 = document.createTextNode('Text2');
+    var input3 = document.createTextNode('Text3');
+
+    td1.appendChild(number);
+    td2.appendChild(input1);
+    td3.appendChild(input2);
+    td4.appendChild(input3);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+
+    table.appendChild(tr);
 
 }
